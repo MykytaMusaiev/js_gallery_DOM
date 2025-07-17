ee4c2cdfe666b8456af0930b5e2b73eabe978ce9
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-17 22:29:04 +0300

    make build

---
+++ index.98ad9f6f.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest("li");if(t){var r=t.querySelector("a");if(r){var n=r.href;document.getElementById("largeImg").setAttribute("src",n)}}});
+//# sourceMappingURL=index.98ad9f6f.js.map
