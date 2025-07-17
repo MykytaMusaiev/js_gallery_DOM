87f1e4550a4ce348360db9d1d7c2b069d397ec59
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-17 21:59:49 +0300

    task solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const listItem = e.target.closest('li');
+  const link = listItem.querySelector('a');
+  const newImageUrl = link.href;
+  const bigImage = document.getElementById('largeImg');
+
+  bigImage.setAttribute('src', newImageUrl);
+});
